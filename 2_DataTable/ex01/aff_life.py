535a8c77a86dd118a893af3085a1b63afff271e1
Clara <prando.c@live.fr>
2025-10-17 11:00:50 +0200

    refactor: clean up load() function and improve readability

---
+++ 2_DataTable/ex01/aff_life.py
@@ -0,0 +1,14 @@
+import matplotlib as plt
+import pandas as pd
+from load_csv import load
+
+def main():
+  """
+  
+  """
+  try:
+    dataFrame = load("life_expectancy_years.csv")
+  except Exception:
+    
+
+
